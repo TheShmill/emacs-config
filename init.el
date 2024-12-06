239ad0a7fc02745e31f0c234f425d2aa81a05565
TheShmill <21013808+TheShmill@users.noreply.github.com>
2024-12-05 23:43:39 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,25 @@
+(defun start/org-babel-tangle-file ()
+  "Tangle config.org into init.el"
+  (when (string-equal (file-name-directory (buffer-file-name))
+		      (expand-file-name user-emacs-directory))
+    (let ((org-confirm-babel-evaluate t))
+      (org-babel-tangle))))
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'start/org-babel-tangle-file)))
+
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(setq mac-command-modifier 'meta
+      mac-option-modifier nil)
+
+(exec-path-from-shell-initialize)
+
+(require 'use-package)
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+(use-package magit
+  :ensure t)
